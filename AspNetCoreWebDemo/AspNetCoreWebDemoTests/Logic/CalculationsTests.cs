d65eedcac7a506532707694fc4f2a489e0ec18a5
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-12-11 10:48:34 +0200

    Lisätty yksikkötestiprojekti Visual Studio -ratkaisuun (solution).

---
+++ AspNetCoreWebDemo/AspNetCoreWebDemoTests/Logic/CalculationsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AspNetCoreWebDemo.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreWebDemo.Logic.Tests
+{
+    [TestClass()]
+    public class CalculationsTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Calculations calc = new Calculations();
+            int a = 100;
+            int b = 200;
+
+            int sum = calc.Sum(a, b);
+            int expected = a + b;
+
+            Assert.AreEqual(expected, sum);
+        }
+    }
+}
