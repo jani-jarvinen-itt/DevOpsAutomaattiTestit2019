99ee8ed8c2ed1ffe1493207a2ba7207ffef7bed4
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-12-11 14:02:00 +0200

    Lisätty Python-luokka ja sille yksikkötesti.

--- Python/Yksikkötesti.py
+++ Python/Yksikkötesti.py
@@ -1,5 +1,10 @@
 import unittest
 
+class OmaJuttu:
+    def oma_metodi(self):
+        print("Moi!")
+        return 5+5
+
 class TestSum(unittest.TestCase):
 
     def test_sum(self):
@@ -8,5 +13,13 @@ class TestSum(unittest.TestCase):
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 3)), 6, "Should be 6")
 
+    def testaa_olio(self):
+        om = OmaJuttu()
+        tulos = om.oma_metodi()
+        self.assertEqual(tulos, 10, "Tuloksen pitäisi olla 10.")
+
+
 if __name__ == '__main__':
+    om = OmaJuttu()
+    om.oma_metodi()
     unittest.main()
